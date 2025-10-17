13865bf2dd5746f6edab629aaa49e48ad1712d73
Marcelo Bravo <marceb95@gmail.com>
2025-10-16 21:19:54 -0300

    Agregando primeros componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./usuario/Login";
 function App() {
 
   return (
     <>
       <h1>Adan frontend</h1>
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />}/>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
