c1053b4eb6323db74bab6522ee16d2e91e4bbef0
Marcelo Bravo <marceb95@gmail.com>
2025-10-28 22:17:08 -0300

    Mejoras en recurso

--- src/cursos/PaginaCurso.jsx
+++ src/cursos/PaginaCurso.jsx
@@ -318,6 +318,15 @@ const NoSectionsMessage = styled.div`
   }
 `;
 
+const Recurso = styled.a`
+  color: #222;
+  &:hover {
+    text-decoration: underline;
+    color: blue;
+    cursor: pointer;
+  }
+`;
+
 const PaginaCurso = () => {
   const handleDescargarMaterial = async (codigo, seccionId, recurso) => {
     const token = localStorage.getItem("token");
@@ -777,7 +786,7 @@ const PaginaCurso = () => {
                                   </button>
                                 </>
                               ) : (
-                                <a onClick={() => verRecurso(recurso.id)} style={{color:'#222', textDecoration: 'underline', cursor: 'pointer'}}>{recurso.nombre === null ? '(null)' : recurso.nombre}</a>
+                                <Recurso onClick={() => verRecurso(recurso.id)} >{recurso.nombre === null ? '(null)' : recurso.nombre}</Recurso>
                               )}
                             </li>
                           ))
