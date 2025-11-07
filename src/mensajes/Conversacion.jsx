a26783b886d81e42604b27abcf772ec5b8074670
Marcelo Bravo <marceb95@gmail.com>
2025-11-07 16:22:08 -0300

    Ajuste imagenes de perfil

--- src/mensajes/Conversacion.jsx
+++ src/mensajes/Conversacion.jsx
@@ -1,9 +1,12 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useMemo } from 'react';
 import styled, {css} from "styled-components";
 import axios from "axios";
 import Spinner from '../general/Spinner';
 
-const Conversacion = ({ conversacionId, idUsuarioActual, esNuevaConversacion, onHandleNuevoChat, onHandleCerrarConversacion }) => {
+const DEFAULT_AVATAR_URL = "/header/avatar.png";
+
+
+const Conversacion = ({ conversacionId, idUsuarioActual, esNuevaConversacion, onHandleNuevoChat, onHandleCerrarConversacion, perfil }) => {
 
     const [mensajes, setMensajes] = useState([]);
     const [participanteNombre, setParticipanteNombre] = useState('Nombre Apellido');
@@ -23,10 +26,14 @@ const Conversacion = ({ conversacionId, idUsuarioActual, esNuevaConversacion, on
                         Authorization: `Bearer ${token}`,
                     },
                 };
+                
+                // Obtener datos del destinatario
                 const response1 = await axios.get(`${urlBase}/usuarios/${conversacionId}`, config);
                 const destinatario = response1.data;
                 setParticipanteNombre(`${destinatario.nombres || ''} ${destinatario.apellidos || ''}`.trim());
-                setParticipanteAvatar(destinatario.fotoPerfil || null);
+                setParticipanteAvatar(destinatario.fotoPerfil || null); // Usar null si no hay foto para el fallback en CSS
+                
+                // Obtener mensajes
                 const response2 = await axios.get(`${urlBase}/mensajes-privados/conversacion/${conversacionId}`, config);
                 setMensajes(response2.data);
             } catch (error) {
@@ -41,8 +48,6 @@ const Conversacion = ({ conversacionId, idUsuarioActual, esNuevaConversacion, on
 
 
     const handleEnviarMensaje = async () => {
-        
-        
         if (mensajeNuevo.trim() === '') return;
 
         try {
@@ -70,51 +75,56 @@ const Conversacion = ({ conversacionId, idUsuarioActual, esNuevaConversacion, on
     }
 
 
-  return (
-    <>
-        <HeaderChat>
-            <BackButton onClick={onHandleCerrarConversacion}>←</BackButton>
-            <TitleChat>{participanteNombre}</TitleChat>
-            <ProfileIcon />
-        </HeaderChat>
-        
-        {cargando && <Spinner />}
-        {!cargando &&
-            <MensajesScrollable>
-                {mensajes.map((msg) => {
-                    const esMensajePropio = idUsuarioActual === msg.idRemitente;
-                    return (
-                        <MensajeBubble 
-                            key={msg.id} 
-                            $propio={esMensajePropio}
-                        >
-                            <ContenidoMensaje propio={esMensajePropio}>
-                                {msg.cuerpoMensaje}
-                            </ContenidoMensaje>
-                            <AvatarChat propio={esMensajePropio} /> 
-                        </MensajeBubble>
-                    );
-                })}
-            </MensajesScrollable>
-        }
-        
-        {!cargando &&
-            <InputArea>
-                <InputMensaje placeholder="Mensaje..." value={mensajeNuevo} 
-                    onChange={(e) => setMensajeNuevo(e.target.value)}
-                    onKeyDown={(e) => {
-                        if (e.key === 'Enter') {
-                            handleEnviarMensaje();
-                        }
-                    }} 
-                />
-                <BotonEnviar onClick={handleEnviarMensaje}>
-                    Enviar
-                </BotonEnviar>
-            </InputArea>
-        }
-    </>
-  )
+    return (
+        <>
+            <HeaderChat>
+                <BackButton onClick={onHandleCerrarConversacion}>←</BackButton>
+                <TitleChat>{participanteNombre}</TitleChat>
+                <ProfileIcon fotoUrl={participanteAvatar || DEFAULT_AVATAR_URL} />
+            </HeaderChat>
+            
+            {cargando && <Spinner />}
+            {!cargando &&
+                <MensajesScrollable>
+                    {mensajes.map((msg) => {
+                        const esMensajePropio = idUsuarioActual === msg.idRemitente;
+                        const avatarParaMensaje = esMensajePropio ? perfil.fotoPerfil : (participanteAvatar || DEFAULT_AVATAR_URL);
+
+                        return (
+                            <MensajeBubble 
+                                key={msg.id} 
+                                $propio={esMensajePropio}
+                            >
+                                <AvatarChat 
+                                    $propio={esMensajePropio} 
+                                    fotoUrl={avatarParaMensaje} 
+                                />
+                                <ContenidoMensaje propio={esMensajePropio}>
+                                    {msg.cuerpoMensaje}
+                                </ContenidoMensaje>
+                            </MensajeBubble>
+                        );
+                    })}
+                </MensajesScrollable>
+            }
+            
+            {!cargando &&
+                <InputArea>
+                    <InputMensaje placeholder="Mensaje..." value={mensajeNuevo} 
+                        onChange={(e) => setMensajeNuevo(e.target.value)}
+                        onKeyDown={(e) => {
+                            if (e.key === 'Enter') {
+                                handleEnviarMensaje();
+                            }
+                        }} 
+                    />
+                    <BotonEnviar onClick={handleEnviarMensaje}>
+                        ▶
+                    </BotonEnviar>
+                </InputArea>
+            }
+        </>
+    )
 }
 
 export default Conversacion;
@@ -147,7 +157,28 @@ const ProfileIcon = styled.div`
     height: 30px;
     border-radius: 50%;
     background-color: #bdbdbd;
-    /* Icono de perfil aquí */
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    
+    background-image: ${props => props.fotoUrl && props.fotoUrl !== DEFAULT_AVATAR_URL ? `url(${props.fotoUrl})` : `url(${DEFAULT_AVATAR_URL})`};
+
+    ${props =>
+        props.$propio
+            ? css`
+                  align-self: flex-end;
+                  flex-direction: row-reverse; 
+              `
+            : css`
+                  align-self: flex-start;
+                  flex-direction: row; 
+              `}
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: #555;
+    font-size: 1.5em;
 `;
 
 const MensajesScrollable = styled.div`
@@ -162,18 +193,19 @@ const MensajesScrollable = styled.div`
 const MensajeBubble = styled.div`
     display: flex;
     margin-bottom: 15px;
-    max-width: 70%;
-    align-items: flex-end;
+    max-width: 80%;
+    align-items: flex-end; 
+    gap: 8px; 
 
     ${props =>
         props.$propio
             ? css`
                   align-self: flex-end;
-                  flex-direction: row-reverse;
+                  flex-direction: row-reverse; 
               `
             : css`
                   align-self: flex-start;
-                  flex-direction: row;
+                  flex-direction: row; 
               `}
 `;
 
@@ -182,14 +214,19 @@ const AvatarChat = styled.div`
     height: 30px;
     border-radius: 50%;
     background-color: #bdbdbd;
-    margin-right: 5px;
+    flex-shrink: 0; 
     
-    ${props =>
-        props.propio &&
-        css`
-            margin-left: 5px;
-            margin-right: 0;
-        `}
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+
+    background-image: ${props => props.fotoUrl && props.fotoUrl !== DEFAULT_AVATAR_URL ? `url(${props.fotoUrl})` : `url(${DEFAULT_AVATAR_URL})`};
+    
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: #3b5998; 
+    font-size: 1.5em; 
 `;
 
 const ContenidoMensaje = styled.div`
@@ -200,6 +237,7 @@ const ContenidoMensaje = styled.div`
     box-shadow: 0 1px 1px rgba(0, 0, 0, 0.05);
     color: #333;
 
+
     ${props =>
         props.propio
             ? css`
@@ -208,6 +246,7 @@ const ContenidoMensaje = styled.div`
               `
             : css`
                   background-color: #fff;
+                  border: 1px solid #e0e0e0; 
                   border-bottom-left-radius: 3px;
               `}
 `;
@@ -240,6 +279,7 @@ const BotonEnviar = styled.button`
     align-items: center;
     justify-content: center;
     font-size: 1.2em;
+    transform: translateX(1px);
 
     &:hover {
         background-color: #2b4480;
